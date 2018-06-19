016e7cd14dfe32dd86dcca756f34b32a4a0d9c44
Suoakira <37964234+Suoakira@users.noreply.github.com>
2018-06-19 17:52:53 +0100

    Add files via upload

--- tetris.js
+++ tetris.js
@@ -13,6 +13,14 @@ const matrix = [
     [0, 1, 0],
 ];
 
+function createMatrix(w, h) {
+    const matrix [];
+    while (h--) {
+        matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
+}
+
 function draw() {
     context.fillStyle = '#000';
     context.fillRect(0, 0, canvas.width, canvas.height);
@@ -32,18 +40,48 @@ function drawMatrix(matrix, offset) {
     });
 }
 
+function playerDrop() {
+    player.pos.y++;
+    dropCounter = 0;
+}
+
+let dropCounter = 0;
+let dropInterval = 1000;
+
+
 let lastTime = 0;
 function update(time = 0) {
     const deltaTime = time - lastTime;
     lastTime = time;
+
+    dropCounter += deltaTime;
+    if (dropCounter > dropInterval) {
+        player.pos.y++;
+        dropCounter = 0;
+    }
+
     console.log(time);
     draw();
     requestAnimationFrame(update);
 }
 
+const arena = createMatrix(12, 20);
+console.log(arena); console.table(arena);
+
 const player = {
     pos: { x: 5, y: 5 },
     matrix: matrix,
 }
+
+document.addEventListener('keydown', event => {
+    console.log(event);
+    if (event.keyCode === 37) {
+        player.pos.x--;
+    } else if (event.keyCode === 39) {
+        player.pos.x++;
+    } else if (event.keyCode === 40) {
+        playerDrop();
+    }
+});
 
 update();
